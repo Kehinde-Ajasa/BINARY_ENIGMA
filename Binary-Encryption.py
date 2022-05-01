71c1ce4cc6840544f84fc59207cb14f0c08733d6
Kehinde_Ajasa <97411395+Kehinde-Ajasa@users.noreply.github.com>
2022-05-01 17:48:41 +0100

    Output function was updated
    The output function of a regular binary code the computer understands is in the 8-binary digit format.
    The new commit focuses on the change. To help re-center the result to a 8-digit binary format.

--- Binary-Encryption.py
+++ Binary-Encryption.py
@@ -1,38 +1,43 @@
-#  This code is used to create an encrypted binary message i.e it helps cocert letters to binary
-word_to_encrypt = input('Enter the word you would like to convert to binary:  ')
+#  This code is used to create an encrypted binary message i.e. it helps covert letters to binary
+
+while True:
+    word_or_sentence_to_encrypt = input('Enter the word you would like to convert to binary:  ')
 
 
-def open_encrypt_database():
-    with open('C:\\Users\\MY PC\\Desktop\\PYTHON PROJECTS\\ASCII-BINARY.WORD converter\\ENCRYPTION-CODE.txt',
-              'r') as encode:
-        encryption_file = encode.readlines()
-        return encryption_file
+    def open_ascii_values_database():
+        """function to help open up the edited format of  the ascii values"""
+
+        with open('C:\\Users\\MY PC\\Desktop\\PYTHON PROJECTS\\ASCII-BINARY.WORD converter\\ENCRYPTION-CODE.txt',
+                  'r') as words_numbers:
+            ascii_values = words_numbers.readlines()  # getting the values of the ascii_values_file in a list format(readlines)
+            return ascii_values
 
 
-open_encrypt_file = (open_encrypt_database())
+    ascii_values_file = (open_ascii_values_database())
 
 
-def get_number_to_convert(word_to_encrypt):
-    global open_encrypt_file
-    for i in word_to_encrypt:
-        index_of_letters = open_encrypt_file.index((i + '\n'))
-        if (i + '\n') in open_encrypt_file:
-            number_to_convert = int(open_encrypt_file[(index_of_letters - 1)])
-            binary_format = (bin(number_to_convert))
-            ascii_format = binary_format[2:]
-            if ascii_format == '100000':
-                print(f'|{ascii_format}|', end='')
+    def convert_to_binary(word_or_sentence_to_encrypt):
+        """function to check the number of a letter in the ascii format and convert to binary"""
+
+        global ascii_values_file  # Calling the variable from outer scope
+        for i in word_or_sentence_to_encrypt:
+            index_of_letters = ascii_values_file.index((i + '\n'))
+            if (i + '\n') in ascii_values_file:
+                number_to_convert = int(ascii_values_file[(index_of_letters - 1)])
+                binary_format = (bin(number_to_convert))
+                ascii_format = binary_format[2:]
+                eight_letter_format = (8 - len(ascii_format)) * '0'
+                print(f'{eight_letter_format + ascii_format}', end='')
                 continue
-            print(f'({ascii_format})', end='')
-            continue
-    return 'ENCRYPTED'
+        return ''
 
 
-print(get_number_to_convert(word_to_encrypt))
+    print(convert_to_binary(word_or_sentence_to_encrypt))
 
-encrypt_new_word = input('Would you like to encrypt another word?(y/n):  ')
-if encrypt_new_word == 'y':
-    new_word = input('Enter new word:  ')
-    print(get_number_to_convert(new_word))
-else:
-    print('PROGRAM FINISHED!!!')
+    encrypt_new_word_or_sentence = input('Would you like to encrypt another word?(y/n):  ').lower()
+    if encrypt_new_word_or_sentence == 'y':
+        new_word = input('Enter new word:  ')
+        print(convert_to_binary(new_word))
+    else:
+        print('PROGRAM FINISHED!!!')
+        break
